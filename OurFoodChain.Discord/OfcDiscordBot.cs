84f6fe8ab10b0e510df17d48ded8e211209e621f
gsemac <gabe.semac@gmail.com>
2021-06-04 21:37:21 -0600

    Integrated database service into bot

--- OurFoodChain.Discord/OfcDiscordBot.cs
+++ OurFoodChain.Discord/OfcDiscordBot.cs
@@ -1,9 +1,12 @@
-﻿using Gsemac.Discord;
+﻿using Discord.Commands;
+using Gsemac.Discord;
 using Gsemac.IO.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OurFoodChain.Data;
 using OurFoodChain.Data.Dal;
+using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace OurFoodChain.Discord {
@@ -31,6 +34,20 @@ namespace OurFoodChain.Discord {
                 .AddScoped<OfcUnitOfWork<OfcDbContext>>();
 
             return base.ConfigureServicesAsync(services);
+
+        }
+        protected override Task ConfigureServicesAsync(IServiceProvider services) {
+
+            services.GetRequiredService<OfcDbContext>().Database.EnsureCreated();
+
+            return base.ConfigureServicesAsync(services);
+
+        }
+        protected override async Task ConfigureCommandsAsync(CommandService commandService, IServiceProvider serviceProvider) {
+
+            await commandService.AddModulesAsync(Assembly.GetExecutingAssembly(), serviceProvider);
+
+            await base.ConfigureCommandsAsync(commandService, serviceProvider);
 
         }
 
