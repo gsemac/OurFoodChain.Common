4d764d4f6bc83b7f22eb4c4a49e240babce358c4
gsemac <gabe.semac@gmail.com>
2021-06-04 20:50:08 -0600

    Added WorldRepositoryExtensions

---
+++ OurFoodChain.Discord/Extensions/WorldRepositoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using OurFoodChain.Data.Dal;
+using OurFoodChain.Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurFoodChain.Discord.Extensions {
+
+    public static class WorldRepositoryExtensions {
+
+        public static Task<World> GetWorldAsync(this IWorldRepository repository, ulong serverId) {
+
+            return Task.FromResult(repository.Find(world => world.DiscordServerId == serverId).FirstOrDefault());
+
+        }
+
+    }
+
+}
