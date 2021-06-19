a5ac4f8467fa1167ab6661fb35ae208fe21a524e
gsemac <gabe.semac@gmail.com>
2021-06-18 19:37:18 -0600

    Removed obsolete IQueryable extension methods

--- OurFoodChain.Discord/Data/Dal/GuildSpeciesRepository.cs
+++ OurFoodChain.Discord/Data/Dal/GuildSpeciesRepository.cs
@@ -3,7 +3,6 @@ using OurFoodChain.Data;
 using OurFoodChain.Data.Dal;
 using OurFoodChain.Data.Extensions;
 using OurFoodChain.Data.Models;
-using OurFoodChain.Discord.Data.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
