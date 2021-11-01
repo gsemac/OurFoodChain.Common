f7778c30035658035d06c4a47e810ba75d705f1d
gsemac <gabe.semac@gmail.com>
2021-10-31 18:43:20 -0600

    Added new ancestor flag to support paraphyletic groupings Paraphyletic groups are generally composed of an ancestor and all descendants minus a few branches, so an exclusion-based flag was chosen rather than an inclusive one.

--- OurFoodChain.Data/Models/AncestorFlags.cs
+++ OurFoodChain.Data/Models/AncestorFlags.cs
@@ -7,8 +7,18 @@ namespace OurFoodChain.Data.Models {
     [Flags]
     public enum AncestorFlags {
         None = 0,
+        /// <summary>
+        /// Used to indicate the mother species for hybrid species.
+        /// </summary>
         Mother = 1,
+        /// <summary>
+        /// Used to indicate the father species for hybrid species.
+        /// </summary>
         Father = 2,
+        /// <summary>
+        /// Used to exclude a branch from paraphyletic groupings.
+        /// </summary>
+        ExcludeBranch = 4,
     }
 
 }
